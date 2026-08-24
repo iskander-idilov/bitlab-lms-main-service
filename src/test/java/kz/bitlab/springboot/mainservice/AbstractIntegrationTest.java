e45a399e092998e663d2742eac2f6cfb8294bcaf
Iskander Idilov <idiloviskander@gmail.com>
2026-08-25 00:10:24 +0500

    fix  AbstractIntegrationTest

--- src/test/java/kz/bitlab/springboot/mainservice/AbstractIntegrationTest.java
+++ src/test/java/kz/bitlab/springboot/mainservice/AbstractIntegrationTest.java
@@ -1,21 +1,15 @@
 package kz.bitlab.springboot.mainservice;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 public abstract class AbstractIntegrationTest {
 
+    @ServiceConnection
     static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
     static {
         postgres.start();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
     }
 }
